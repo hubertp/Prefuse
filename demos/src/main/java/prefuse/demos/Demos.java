b1216bf6a2d7d8a864685738bd0f9da312b211f4
Sciss <contact@sciss.de>
2011-07-11 16:20:19 +0100

    demo cleanup

---
+++ demos/src/main/java/prefuse/demos/Demos.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2004-2006 Regents of the University of California.
+ * See "license-prefuse.txt" for licensing terms.
+ */
+package prefuse.demos;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.lang.reflect.*;
+
+/**
+ * A simple wrapper for the demo applications.
+ * 
+ * @author Hanns Holger Rutz
+ */
+public class Demos implements Runnable {
+	private static final Class[] clz = {
+		AggregateDemo.class,
+		Congress.class,
+		DataMountain.class,
+		FisheyeMenu.class,
+		GraphView.class,
+		RadialGraphView.class,
+		ScatterPlot.class,
+		TreeMap.class,
+		TreeView.class,
+		ZipDecode.class
+	};
+	
+    public static void main(String[] argv) {
+		EventQueue.invokeLater( new Demos() );
+    }
+    
+	public void run() {
+		final JFrame f = new JFrame( "Prefuse Demos" );
+		f.setResizable( false );
+		final Container cp = f.getContentPane();
+//		cp.setLayout( new BoxLayout( cp, BoxLayout.Y_AXIS ));
+		cp.setLayout( new GridLayout( clz.length, 1 ));
+		for( int i = 0; i < clz.length; i++ ) {
+			final Class c = clz[ i ];
+			final String cn = c.getName();
+			final String name = cn.substring( cn.lastIndexOf( '.' ) + 1 ); 
+			final JButton b = new JButton( new AbstractAction( name ) {
+				public void actionPerformed( ActionEvent e ) {
+					try {
+						final Method m = c.getMethod( "main", new Class[] { String[].class });
+						m.invoke( null, new Object[] { null });
+					} catch( Throwable e1 ) {
+						e1.printStackTrace();
+					}
+				}
+			});
+			cp.add( b );
+			b.setFocusable( false );
+			b.putClientProperty( "JButton.buttonType", "bevel" );
+		}
+		f.pack();
+		f.setLocationRelativeTo( null );
+		f.setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
+		f.setVisible( true );
+	}
+}
--- project/build/Prefuse.scala
+++ project/build/Prefuse.scala
@@ -1,5 +1,21 @@
 import sbt._
 
-class PrefuseProject(info: ProjectInfo) extends DefaultProject(info) {
-   override def javaCompileOptions = super.javaCompileOptions ++ javaCompileOptions("-Xlint:unchecked", "-source", "1.4")
+class PrefuseProject(info: ProjectInfo) extends ParentProject(info) {
+   lazy val core  = project("core", "core", new Core(_))
+   lazy val demos = project("demos", "demos", new Demos(_), core)
+   
+   class Core(info: ProjectInfo) extends DefaultProject(info) {
+      override def javaCompileOptions = super.javaCompileOptions ++ 
+         javaCompileOptions("-Xlint:unchecked", "-source", "1.4")
+   }
+
+   class Demos(info: ProjectInfo) extends DefaultProject(info) {
+      override def javaCompileOptions = super.javaCompileOptions ++ 
+         javaCompileOptions("-Xlint:unchecked", "-source", "1.4")
+      
+      def extraResources = "data"
+      override def mainResources = super.mainResources +++ extraResources
+         
+      override def mainClass = Some("prefuse.demos.Demos")
+   }
 }
