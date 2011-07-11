09537707db55e227b6cf3e5cd5890bcb24764377
Sciss <contact@sciss.de>
2011-07-11 18:07:09 +0100

    ok, this is bit of a hackery, but sbt publish(-local) seems to do what i want now.

--- project/build/Prefuse.scala
+++ project/build/Prefuse.scala
@@ -1,14 +1,16 @@
 import sbt._
 
 class PrefuseProject(info: ProjectInfo) extends ParentProject(info) {
-   lazy val core  = project("core", "core", new Core(_))
-   lazy val demos = project("demos", "demos", new Demos(_), core)
-   
+   lazy val core  = project("core", "prefuse", new Core(_))
+   lazy val demos = project("demos", "prefuse-demos", new Demos(_), core)
+      
    class Core(info: ProjectInfo) extends DefaultProject(info) {
       override def javaCompileOptions = super.javaCompileOptions ++ 
          javaCompileOptions("-Xlint:unchecked", "-source", "1.4")
          
       val junit = "junit" % "junit" % "3.8.1" % "test->default"
+
+      override def disableCrossPaths = true
    }
 
    class Demos(info: ProjectInfo) extends DefaultProject(info) {
@@ -19,5 +21,12 @@ class PrefuseProject(info: ProjectInfo) extends ParentProject(info) {
       override def mainResources = super.mainResources +++ extraResources
          
       override def mainClass = Some("prefuse.demos.Demos")
+      override def disableCrossPaths = true
    }
+
+   def doNothing = task { None }
+   override def publishLocalAction = doNothing
+   override def deliverLocalAction = doNothing
+   override def publishAction = doNothing
+   override def deliverAction = doNothing
 }
